5961b7729bf079b5931bdd5af198969dcdfee892
saihari08 <bsaihari08@gmail.com>
2024-06-06 17:20:11 +0530

    Add files via upload

---
+++ second react project/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body id="indexbody"> 
+     <h1 id="title">RRR</h1>
+ <div>
+      <h2>Poster</h2>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGO355B2uHufftmYd4xYf5qYHhguUWedVzTA&s"/>
+</div>  
+
+      <h2>Trailer</h2>
+      <iframe width="400" height="315" src="https://www.youtube.com/embed/2_BkCz3OnlY?si=YMkBw2lcMrBppwjk"></iframe>
+
+<div > 
+    <h2>Glimpse</h2>
+     <iframe width="400" height="315" src="https://www.youtube.com/watch?v=sjvMJumvKbQ"></iframe>
+</div>
+    </body>
+   
+  );
+}
+
+export default App;
